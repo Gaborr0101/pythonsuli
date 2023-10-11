93a513f36d343e75eb8b4246f7df7318b74ba024
Gyarmati Gábor <gyarmatig22@ADY-NAGYATAD.SULINET.HU>
2023-10-11 08:44:17 +0200

    10.11

--- Programozás/gyak.10.11..py
+++ Programozás/gyak.10.11..py
@@ -1,1 +1,31 @@
-print("oksi")
+
+
+lista=[]
+
+beker="menjébe"
+
+while beker !="":
+    beker=input("adj meg valamit: ")
+    if beker != "":
+        lista.append(beker)
+
+print(lista)
+utolso=lista[-3:]
+utolso.sort()
+
+lista.sort()
+
+for e in lista:
+    print(e)
+
+for e in lista[-3:]:
+    print(e)
+
+for e in utolso:
+    print(e)
+
+
+
+#hf hozzunk létre egy listát, kérj be egy számot, ha lehetséges arra helyre szurj be egy másik adatot     
+
+
