d62908bd51588d93a9346740d4b0be78196c7043
Gyarmati Gábor <gyarmatig22@ady-nagyatad.sulinet.hu>
2025-01-29 10:36:19 +0100

    asd

--- memoria/script.js
+++ memoria/script.js
@@ -1,6 +1,10 @@
 const kepek=["images.jpg","kosar1.jpg","kosar2.jpg","longma%20pong.jpg","malna.jpg","malna2.jpg","mc.jpg","mc2.jpg","pong.jpg","solar2.jpg","tabla%20598%20a%20nyil.jpg","598a3ecb-4a56-4e02-878e-c386ade6a70b.jpg","adysuli.jpg","adysuli2.jpg","alma.jpg","almafa.jpeg","almafa.jpg","assasins 2.jpg","assasins.jpg","atadsolar.jpg","Cigarette.jpg","digitalis-merleg-cigarettasdoboz-alakban-cg-200-200-g-001-g.jpg","gerven.jpg","gerven2.jpg"];
 const kepdb=24;
 let pakli=[];
+let felforditva=[];
+
+
+
 
 function init()
 {
@@ -13,7 +17,24 @@ function init()
         //temp.style.backgroundImage=`url(${kepek[i]})`;
         //temp.style.backgroundImage="url("+kepek[Math.floor(i/2)]+")";
         temp.dataset.kep="url("+kepek[seged]+")";
-
+        
+        temp.onclick=function()
+        {
+            if (felforditva.length<2)
+            {
+                if(!(felforditva.length===1 && felforditva[0]===this))
+                {
+                    this.style.backgroundImage=this.dataset.kep;
+                    felforditva.push(this);
+                }
+               
+            }
+            if (felforditva.length>=2)
+            {
+                egyenloe();
+            }
+            
+        }
 
         pakli.push(temp);
         //document.getElementById("asztal").appendChild(temp);
@@ -26,15 +47,68 @@ function init()
     }
     
     
-    pakli=shuffleArray(pakli);
-    for (let i=0; i<pakli.length; i++)
+function egyenloe()
+{
+    //console.log(felforditva);
+    if (felforditva[0].style.backgroundImage==felforditva[1].style.backgroundImage)
     {
-        document.getElementById("asztal").appendChild(pakli[i]);
+        felforditva[0].oclick="";
+        felforditva[1].oclick="";
+        felforditva=[];
+        checkgameover();
+
     }
+    else
+    {   
+
+        setTimeout(visszafordit,2000)
+
+    }
+}
+
+
+function checkgameover()
+{
+    let gameover=true;
+     for(let i=0;i<pakli.length;i++)
+     {
+        //gameover&=pakli[i].style.backgroundImage!=="";
+        gameover = gameover && pakli[i].style.backgroundImage!=="";
+     }
+
+    if (gameover)
+    {
+        console.log("vége");
+        let end=document.createElement("div");
+        end.classList.add("gameover")
+        end.innerHTML="Vége a játéknak";
+        document.body.appendChild(end);
+    }
+}
+
+pakli=shuffleArray(pakli);
+for (let i=0; i<pakli.length; i++)
+{
+    document.getElementById("asztal").appendChild(pakli[i]);
+
+}
+
+    
 
 
 }
 
+
+function visszafordit()
+{
+    for(let i=0; i<felforditva.length; i++)
+    {
+        felforditva[i].style.backgroundImage="";
+    }
+
+    felforditva=[];
+
+}
 function shuffleArray(array) 
 {
     for (let i = array.length - 1; i >= 1; i--) 
