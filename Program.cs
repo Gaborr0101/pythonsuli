ba7e0adfb839f65caf41645f01c5bb0cd731222a
Gyarmati Gábor <gyarmatig22@ady-nagyatad.sulinet.hu>
2025-02-05 14:13:44 +0100

    asd

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace fealdat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("felajanlas.txt");
+            List<adatok> felajanlasok = new List<adatok>();
+            int agyasDb = int.Parse(sorok[0]);
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                felajanlasok.Add(new adatok(sorok[i], i));
+            }
+            Console.WriteLine($"2. feladat:\nA felajanlasok száma: {felajanlasok.Count}");
+
+            Console.WriteLine("3. feladat");
+
+            Console.WriteLine("4. feladat");
+            Console.Write("Adja meg az ágyás sorszámát! ");
+            int agyas = int.Parse(Console.ReadLine());
+            int darab = 0;
+            string szin = "#";
+            List<string> szinek = new List<string>();
+            for (int i = 0; i < felajanlasok.Count; i++)
+            {
+                if (felajanlasok[i].benneVanEz(agyas))
+                {
+                    darab++;
+                    if (darab == 1)
+                    {
+                        szin = felajanlasok[i].szin;
+                    }
+                    if (!szinek.Contains(felajanlasok[i].szin))
+                    {
+                        szinek.Add(felajanlasok[i].szin);
+                    }
+                }
+            }
+            Console.WriteLine("A felajánlók száma: {0}", darab);
+            Console.WriteLine("A virágágyás szine, ha csak az első ültet: {0}", szin);
+            Console.WriteLine("A virágágyás színei: {0}", String.Join(" ", szinek));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ feladat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fealdat1
+{
+    internal class adatok
+    {
+        public int kezd;
+        public int veg;
+        public string szin;
+        public int sorszam;
+
+        public adatok(string sor, int sorszam)
+        {
+            string[] vag = sor.Split(" ");
+            kezd = int.Parse(vag[0]);
+            veg = int.Parse(vag[1]);
+            szin = vag[2];
+            this.sorszam = sorszam;
+        }
+
+        public bool kaputKetOldal
+        {
+            get
+            {
+                return kezd > veg;
+            }
+        }
+
+        public bool benneVanEz(int sorszam)
+        {
+            return (sorszam >= kezd && sorszam <= veg)
+                || (kaputKetOldal && (kezd <= sorszam || veg >= sorszam));
+        }
+    }
+}
